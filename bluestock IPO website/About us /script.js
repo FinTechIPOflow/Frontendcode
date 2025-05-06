235126c4749e8b4db2cc46ef83b121c5b7125777
NIDURAM RAKESH <125959195+rakesh103706@users.noreply.github.com>
2025-05-06 19:14:32 +0530

    script.js

---
+++ bluestock IPO website/About us /script.js
@@ -0,0 +1,7 @@
+// FAQ toggle
+document.querySelectorAll('.faq-item button').forEach(button => {
+  button.addEventListener('click', () => {
+    const content = button.nextElementSibling;
+    content.style.display = (content.style.display === 'block') ? 'none' : 'block';
+  });
+});
