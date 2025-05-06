9d5486a77c654c1e59c12f2d1cb5c7ccc4543faa
NIDURAM RAKESH <125959195+rakesh103706@users.noreply.github.com>
2025-05-06 18:41:15 +0530

    Create script.js

---
+++ bluestock IPO website/component 6/script.js
@@ -0,0 +1,8 @@
+document.getElementById("joinBtn").addEventListener("click", () => {
+  alert("Redirecting to the Bluestock Community!");
+});
+
+document.getElementById("openChat").addEventListener("click", (e) => {
+  e.preventDefault();
+  alert("Chat feature will be available soon!");
+});
